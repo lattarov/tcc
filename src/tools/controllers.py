b1c6add08adddcd0d656ce664fda8f972c612946
Victor Lattaro Volpini <lattaro@pop-os.home>
2024-08-18 16:57:32 +0200

    feat: separated controllers into different files

--- src/tools/controllers.py
+++ src/tools/controllers.py
@@ -1,37 +1,167 @@
 """
 Contains different controller implementations.
+
 """
 
+import control.matlab
+import cvxpy as cp
 import numpy as np
 
-from control.matlab import ss, lsim, feedback, lqr, step
 from tools.model import sys
 
-# Implement PID controller manually
-class PIDController:
-    def __init__(self, Kp, Ki, Kd, setpoint):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.Kd = Kd
-        self.setpoint = setpoint
-        self.integral = 0
-        self.prev_error = 0
 
-    def control(self, measurement, dt):
-        error = self.setpoint - measurement
-        self.integral += error * dt
-        derivative = (error - self.prev_error) / dt
-        self.prev_error = error
-        return self.Kp * error + self.Ki * self.integral + self.Kd * derivative
+class PIDController():
+    """
+    Implements a classical PID controller.
 
 
 
-# Model Predictive Control (MPC) using LQR
-Q = np.diag([1, 1, 10, 100])  # State cost
-R = np.array([[0.01]])  # Input cost
+    Attributes
+    ----------
+    gains : dict
+        contains gains for proportional, integral and derivative gains.
 
-K, _, _ = lqr(sys.A, sys.B, Q, R)
+    _error_integral : np.float64
+        integral of error from start time until step N-1.
 
-# Closed-loop system with LQR (MPC approximation)
-A_cl = sys.A - sys.B @ K
-sys_mpc = ss(A_cl, sys.B, sys.C, sys.D)
+    _prev_error : np.float64
+        error value at time step N-1.
+
+    duration : int
+        Total number of timesteps in the simulation.
+
+    Exemples
+    ----------
+    gains = {"Kp" : 5, "Kd" : 3, "Ki" : 0.00005}
+    controller = PIDController(gains)
+
+    """
+
+    def __init__(self, gains: dict) -> None:
+        self.gains = gains
+        self._error_integral = np.float64(0)
+        self._prev_error = np.float64(0)
+
+    def control_step(self, setpoint, measurement, dt):
+        error = np.float64(setpoint - measurement)
+        self._error_integral += np.float64(error * dt)
+        error_derivative = np.float64((error - self._prev_error) / dt)
+        self._prev_error = error
+
+        return self.gains["Kp"] * error + \
+            self.gains["Ki"] * self._error_integral + \
+            self.gains["Kd"] * error_derivative
+
+
+class MPCController():
+    """
+    Implements a model predictive controller.
+
+
+
+    Attributes
+    ----------
+    q_matrix : np.diag
+        TODO: Description of q_matrix.
+
+    r_matrix : np.diag
+        TODO: Description of r_matrix.
+
+    sys : control.matlab.ss
+        State space model of the system.
+
+    x0 : np.array
+        Array containing the initial positions for each state of the state
+        space model to be controlled.
+
+    duration : int
+        Total number of timesteps in the simulation.
+
+    Exemples
+    ----------
+    controller = MPCController(np.diag([1, 1, 10, 100]), np.array([[0.01r]),
+                            np.array([0, 0, np.pi/4, 0]), 10)
+
+    """
+
+    def __init__(self, q_matrix: np.diag,
+                 r_matrix: np.array,
+                 sys: control.matlab.ss,
+                 x0: np.array,
+                 duration: int) -> None:
+        self.q_matrix = q_matrix
+        self.r_matrix = r_matrix
+        self.sys = sys
+        self.x0 = x0
+        self.duration = duration
+
+        self.u_max = 10  # Maximum control input (force)
+        self.u_min = -10  # Minimum control input (force)
+
+        self.x_mpc = np.zeros((duration, 4))
+        self.x_mpc[0] = x0
+        self.u_mpc_array = []
+
+        self._get_closed_loop_gain_matrix()
+        self._get_closed_loop_state_matrix()
+        self._get_closed_loop_model()
+
+    def _get_closed_loop_gain_matrix(self, ):
+        self.k_matrix, _, _ = control.matlab.lqr(self.sys.A,
+                                                 self.sys.B,
+                                                 self.q_matrix, self.r_matrix)
+
+    def _get_closed_loop_state_matrix(self, ):
+        self.a_matrix_closed_loop = self.sys.A - self.sys.B @ self.k_matrix
+
+    def _get_closed_loop_model(self, ):
+        self.closed_loop_model = control.matlab.ss(
+            self.a_matrix_closed_loop, sys.B, sys.C, sys.D)
+
+    def control_step(self, i, dt, N=10,):
+        """
+        Calculates the control value for the Nth time step.
+
+        Parameters
+        ----------
+        i : int
+            Simulation step number.
+        dt : int
+            Time step duration in seconds.
+        N : int = 10
+            Prediction horizon for the MPC.
+
+        WARNING: control steps should go up to duration-1 !
+
+        Exemples
+        ----------
+
+        """
+
+        # Define optimization variables
+        x = cp.Variable((4, N+1))
+        u = cp.Variable((1, N))
+
+        # Define cost function
+        cost = 0
+        r_matrix = np.array([[0.01]])  # Input cost, not used.
+        constraints = [x[:, 0] == self.x_mpc[i]]
+        for k in range(N):
+            cost += cp.quad_form(x[:, k], self.q_matrix) + \
+                cp.quad_form(u[:, k], r_matrix)
+            constraints += [x[:, k+1] == self.sys.A @ x[:, k] + self.sys.B @ u[:, k],
+                            self.u_min <= u[:, k], u[:, k] <= self.u_max]
+
+        # Solve the optimization problem
+        prob = cp.Problem(cp.Minimize(cost), constraints)
+        prob.solve()
+
+        # Apply the first control input
+        u_mpc = u.value[:, 0]  # Get the first control input
+        self.u_mpc_array.append(u_mpc)
+
+        # Simulate the system for the next time step
+        t_sim = [0, dt]
+        u_sim = np.array([u_mpc, u_mpc])  # Control input for simulation
+        _, x_next, _ = control.matlab.lsim(sys, u_sim, t_sim, self.x_mpc[i])
+        self.x_mpc[i+1] = x_next[-1]
